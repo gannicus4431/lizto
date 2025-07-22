f155d1c1cc19155f3a8094cc0e734e0d9e3ffcef
gannicus4431 <gannicus4431@gmail.com>
2025-07-22 13:11:10 +0800

    a

--- components/menu/Banner1.tsx
+++ components/menu/Banner1.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Banner1() {
   return (
     <section className="relative">
--- components/menu/Banner2.tsx
+++ components/menu/Banner2.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Banner2() {
   return (
     <section className="bg-cream-50" style={{ backgroundColor: '#faf7f0' }}>
